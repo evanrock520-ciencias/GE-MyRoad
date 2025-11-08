43b74114fab23b83be24476f273c0d6b846104df
evanrock520-ciencias <evanrock520l@ciencias.unam.mx>
2025-11-08 12:55:07 -0600

    feat: add InOut file

---
+++ CppLearning/Essentials/InOut.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int number;
+
+int main()
+{
+    cout <<  "Please enter a number" << endl;
+    cin >> number;
+    cout << "Your number is : " << number << endl;
+    return 0;
+}
+
